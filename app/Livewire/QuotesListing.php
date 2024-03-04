58cbecb1dd94020feb64839a23162e5209598f32
Donald <donaldovcharov@gmail.com>
2024-03-04 22:36:20 +0200

    Init project with jetstream

---
+++ app/Livewire/QuotesListing.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class QuotesListing extends Component
+{
+    public function render()
+    {
+        return view('livewire.quotes-listing');
+    }
+}
